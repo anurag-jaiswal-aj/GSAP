035c158b918b772ccc2ad6f485180e0ed02fb7e4
Anurag Jaiswal <janurag582004@gmail.com>
2024-06-03 14:15:42 +0530

    .

--- Day_07/script6.js
+++ Day_07/script6.js
@@ -1,4 +1,31 @@
+function marqueAnimation() {
 
-window.addEventListener("wheel",function(dets){
-     
-})
+    window.addEventListener("wheel", function (dets) {
+        if (dets.deltaY > 0) {
+            gsap.to(".marque", {
+                transform: 'translateX(-200%)',
+                duration: 3,
+                repeat: -1,
+                ease: "none"
+            })
+
+            gsap.to(".marque img", {
+                rotate: 0
+            })
+
+        } else {
+            gsap.to(".marque", {
+                transform: 'translateX(0%)',
+                duration: 3,
+                repeat: -1,
+                ease: "none"
+            })
+
+            gsap.to(".marque img", {
+                rotate: 180
+            })
+        }
+    })
+}
+
+marqueAnimation()
