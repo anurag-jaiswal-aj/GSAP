563c3e98d9a30a40da6384028e213658ea48168e
Anurag Jaiswal <janurag582004@gmail.com>
2024-06-04 01:13:22 +0530

    .

--- Day_08/script7.js
+++ Day_08/script7.js
@@ -1,1 +1,27 @@
 
+var tl = gsap.timeline()
+
+tl.from("nav h1,nav h4,nav button",{
+    y:-30,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:0.15
+})
+
+tl.from(".center-part1 h1",{
+    x:-300,
+    opacity:0,
+    duration:0.5
+})
+
+tl.from(".center-part1 p",{
+    x:-100,
+    opacity:0,
+    duration:0.4
+})
+
+tl.from(".center-part1 button",{
+    opacity:0,
+    duration:0.4
+})
