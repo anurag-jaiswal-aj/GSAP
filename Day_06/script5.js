9419296df3ecf13e5632119be7b234859c01a8f1
Anurag Jaiswal <janurag582004@gmail.com>
2024-06-03 13:03:57 +0530

    .

--- Day_06/script5.js
+++ Day_06/script5.js
@@ -1,1 +1,43 @@
+function breakTheText() {
 
+    var h1 = document.querySelector("h1")
+    var h1Text = h1.textContent
+    var splittedText = h1Text.split("")
+    var halfValue = splittedText.length/2
+
+    var clutter = ""
+
+    splittedText.forEach(function (elem, idx) {
+        if(idx<halfValue){
+        clutter += `<span class="a">${elem}</span>`
+        }
+        else{
+        clutter += `<span class="b">${elem}</span>`
+
+        }
+    })
+
+    h1.innerHTML = clutter
+
+}
+
+breakTheText()
+
+
+gsap.from("h1 .a", {
+    y: 80,
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.5,
+    stagger: 0.15,
+    // stagger:-0.15,
+})
+
+gsap.from("h1 .b", {
+    y: 80,
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.5,
+    stagger: -0.15,
+    // stagger:-0.15,
+})
