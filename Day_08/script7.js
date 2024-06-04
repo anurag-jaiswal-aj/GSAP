4b42aab4cdcdb956f64546996ee5a1d8d5616a01
Anurag Jaiswal <janurag582004@gmail.com>
2024-06-05 00:03:19 +0530

    .

--- Day_08/script7.js
+++ Day_08/script7.js
@@ -1,10 +1,11 @@
-
+function page1Animation(){
+    
 var tl = gsap.timeline()
 
 tl.from("nav h1,nav h4,nav button",{
     y:-30,
-    duration:1,
-    delay:1,
+    duration:0.7,
+    delay:0.5,
     opacity:0,
     stagger:0.15
 })
@@ -13,7 +14,7 @@ tl.from(".center-part1 h1",{
     x:-300,
     opacity:0,
     duration:0.5
-})
+},"-=0.3")
 
 tl.from(".center-part1 p",{
     x:-100,
@@ -25,3 +26,67 @@ tl.from(".center-part1 button",{
     opacity:0,
     duration:0.4
 })
+
+tl.from(".center-part2 img",{
+    opacity:0,
+    duration:0.5,
+    x:200
+},"-=0.7")
+
+tl.from(".section1bottom img",{
+    opacity:0,
+    y:30,
+    stagger:0.15,
+    duration:0.6
+})
+}
+page1Animation()
+
+
+function page2Animation(){
+
+var tl2 = gsap.timeline({
+    scrollTrigger:{
+        trigger:".section2",
+        scroller:"body",
+        // markers:"true",
+        start:"top 50%",
+        scrub:2,
+        end:"top -20%"
+    }
+})
+
+
+tl2.from(".services",{
+    y:80,
+    opacity:0,
+    duration:0.5
+})
+
+tl2.from(".elem.line1.left",{
+    x:-300,
+    opacity:0,
+    duration:1
+},"anim")
+
+tl2.from(".elem.line1.right",{
+    x:300,
+    opacity:0,
+    duration:1
+},"anim")
+
+tl2.from(".elem.line2.left",{
+    x:-300,
+    opacity:0,
+    duration:1
+},"aj")
+
+tl2.from(".elem.line2.right",{
+    x:300,
+    opacity:0,
+    duration:1
+},"aj")
+
+
+}
+page2Animation()
